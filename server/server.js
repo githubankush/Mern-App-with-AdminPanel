bb7d015259fa32d8f4f6135c8a09cb40d1fcebbf
githubankush <ankushkumayu04@gmail.com>
2025-09-02 22:48:52 +0530

    client url added in server side

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, //access-control-allow-credentials:true
 }
